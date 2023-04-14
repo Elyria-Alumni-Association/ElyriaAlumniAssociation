d61f951737166c2a825166ab828a8353a7b46833
Michael <lengen.6@gmail.com>
2023-04-14 16:35:01 -0400

    Data recovery working

--- Controllers/DeletedAlumniController.cs
+++ Controllers/DeletedAlumniController.cs
@@ -1,4 +1,5 @@
 ﻿using ElyriaAlumniAssociation.Data;
+using ElyriaAlumniAssociation.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
@@ -29,42 +30,70 @@ namespace ElyriaAlumniAssociation.Controllers
             return View(results);
         }
 
-        [Authorize]
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> RestoreAlumni(List<DeletedAlumnus>? alumni)
         {
-            if (id == null || _deletedContext.DeletedAlumnus == null)
+            List<DeletedAlumnus> alumniToRestore = new List<DeletedAlumnus>();
+            foreach (var alumnus in alumni)
             {
-                return NotFound();
-            }
-
-            var alumnus = await _deletedContext.DeletedAlumnus
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (alumnus == null)
-            {
-                return NotFound();
-            }
-
-            return View(alumnus);
-        }
-
-        // POST: Alumni/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_deletedContext.DeletedAlumnus == null)
-            {
-                return Problem("Entity set 'DeletedAlumnusDbContext.DeletedAlumnus'  is null.");
-            }
-            var alumnus = await _deletedContext.DeletedAlumnus.FindAsync(id);
-         
-            if (alumnus != null)
-            {
-                _deletedContext.DeletedAlumnus.Remove(alumnus);
+                if (alumnus.Selected)
+                {
+                    var foundAlumnus = await _deletedContext.DeletedAlumnus.FirstOrDefaultAsync(x => x.Id == alumnus.Id);
+                    var restoredAlumnus = ConvertDeletedAlumnusToAlumnus(foundAlumnus);
+                    _context.Alumnus.Add(restoredAlumnus);
+                    _deletedContext.DeletedAlumnus.Remove(_deletedContext.DeletedAlumnus.Find(alumnus.Id));
+                }
             }
 
             await _deletedContext.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DeleteAlumni(List<DeletedAlumnus>? alumni)
+        {
+            List<DeletedAlumnus> alumniToDelete = new List<DeletedAlumnus>();
+            foreach (var alumnus in alumni)
+            {
+                if (alumnus.Selected)
+                {
+                    alumniToDelete.Add(alumnus);
+                }
+            }
+
+            _deletedContext.DeletedAlumnus.RemoveRange(alumniToDelete);
+            await _deletedContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private Alumnus ConvertDeletedAlumnusToAlumnus(DeletedAlumnus alumnus)
+        {
+            Alumnus restoredAlumnus = new Alumnus();
+            restoredAlumnus.FirstName = alumnus.FirstName;
+            restoredAlumnus.LastName = alumnus.LastName;
+            restoredAlumnus.MiddleInitial = alumnus.MiddleInitial;
+            restoredAlumnus.LastNameAtGraduation = alumnus.LastNameAtGraduation;
+            restoredAlumnus.School = alumnus.School;
+            restoredAlumnus.GraduationYear = alumnus.GraduationYear;
+            restoredAlumnus.StreetAddress = alumnus.StreetAddress;
+            restoredAlumnus.City = alumnus.City;
+            restoredAlumnus.State = alumnus.State;
+            restoredAlumnus.Country = alumnus.Country;
+            restoredAlumnus.PostalCode = alumnus.PostalCode;
+            restoredAlumnus.EmailAddress = alumnus.EmailAddress;
+            restoredAlumnus.PhoneNumber = alumnus.PhoneNumber;
+            restoredAlumnus.ScholasticAward = alumnus.ScholasticAward;
+            restoredAlumnus.Athletics = alumnus.Athletics;
+            restoredAlumnus.Theatre = alumnus.Theatre;
+            restoredAlumnus.Band = alumnus.Band;
+            restoredAlumnus.Choir = alumnus.Choir;
+            restoredAlumnus.Clubs = alumnus.Clubs;
+            restoredAlumnus.ClassOfficer = alumnus.ClassOfficer;
+            restoredAlumnus.ROTC = alumnus.ROTC;
+            restoredAlumnus.OtherActivities = alumnus.OtherActivities;
+            restoredAlumnus.CurrentStatus = alumnus.CurrentStatus;
+            restoredAlumnus.Selected = false;
+
+            return restoredAlumnus;
         }
     }
 }
