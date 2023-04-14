1d564628be89254230178e48f55330fdbcde982a
Michael <lengen.6@gmail.com>
2023-04-13 22:17:57 -0400

    Got deleted data index page and individual permanent delete working

--- Controllers/DeletedAlumniController.cs
+++ Controllers/DeletedAlumniController.cs
@@ -1,4 +1,5 @@
 ﻿using ElyriaAlumniAssociation.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -11,19 +12,59 @@ namespace ElyriaAlumniAssociation.Controllers
         private readonly ApplicationDbContext _context;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<DeletedAlumniController> _logger;
+        private readonly DeletedAlumnusDbContext _deletedContext;
 
         public DeletedAlumniController(ApplicationDbContext context, UserManager<IdentityUser> userManager, IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
-            ILogger<DeletedAlumniController> logger)
+            ILogger<DeletedAlumniController> logger, DeletedAlumnusDbContext deletedContext)
         {
             _context = context;
             _userManager = userManager;
             _logger = logger;
+            _deletedContext = deletedContext;
         }
         public async Task <IActionResult> Index()
         {
-            var results = await _context.DeletedAlumnus.ToListAsync();
+            var results = await _deletedContext.DeletedAlumnus.ToListAsync();
             return View(results);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _deletedContext.DeletedAlumnus == null)
+            {
+                return NotFound();
+            }
+
+            var alumnus = await _deletedContext.DeletedAlumnus
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (alumnus == null)
+            {
+                return NotFound();
+            }
+
+            return View(alumnus);
+        }
+
+        // POST: Alumni/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_deletedContext.DeletedAlumnus == null)
+            {
+                return Problem("Entity set 'DeletedAlumnusDbContext.DeletedAlumnus'  is null.");
+            }
+            var alumnus = await _deletedContext.DeletedAlumnus.FindAsync(id);
+         
+            if (alumnus != null)
+            {
+                _deletedContext.DeletedAlumnus.Remove(alumnus);
+            }
+
+            await _deletedContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
